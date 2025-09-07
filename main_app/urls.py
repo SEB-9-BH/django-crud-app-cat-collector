09b50139dcbac941f33a9a5f7ae05c45316010d7
Kristina DeSilva <97710440+kavdesilva@users.noreply.github.com>
2025-09-07 16:54:10 +0300

    solution pt 1

--- main_app/urls.py
+++ main_app/urls.py
@@ -7,4 +7,11 @@ urlpatterns = [
     # Routes will be added here
     path('', views.home, name='home'),
     path('about/', views.about, name='about'),
+    path('cats/', views.cat_index, name='cat_index'),
+    path('cats/<int:cat_id>/', views.cat_detail, name='cat_detail'),
+    path('cats/create/', views.CatCreate.as_view(), name='cat_create'),
+    path('cats/<int:pk>/update/', views.CatUpdate.as_view(), name='cat_update'),
+    path('cats/<int:pk>/delete/', views.CatDelete.as_view(), name='cat_delete'),
+
+
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from .models import Cat
 
 # Create your views here.
 def home(request):
@@ -9,3 +11,24 @@ def home(request):
 
 def about(request):
     return render(request, 'about.html')
+
+def cat_index(request):
+    cats = Cat.objects.all()
+    return render(request, 'cats/index.html', {'cats': cats})
+
+def cat_detail(request, cat_id):
+    cat = Cat.objects.get(id=cat_id)
+    return render(request, 'cats/detail.html', {'cat': cat})
+
+class CatCreate(CreateView):
+    model = Cat
+    fields = '__all__'
+
+class CatUpdate(UpdateView):
+    model = Cat
+    # Let's disallow the renaming of a cat by excluding the name field!
+    fields = ['breed', 'description', 'age']
+
+class CatDelete(DeleteView):
+    model = Cat
+    success_url = '/cats/'
