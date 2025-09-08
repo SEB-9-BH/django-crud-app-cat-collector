10e7e0da25e1b99791214e1a9b0cb5c0b0ac02da
Kristina DeSilva <97710440+kavdesilva@users.noreply.github.com>
2025-09-08 13:04:55 +0300

    part 4 complete

--- main_app/models.py
+++ main_app/models.py
@@ -1,6 +1,13 @@
 from django.db import models
 from django.urls import reverse
 
+MEALS = (
+    ('B', 'Breakfast'),
+    ('L', 'Lunch'),
+    ('D', 'Dinner')
+)
+
+
 class Cat(models.Model):
     name = models.CharField(max_length=100)
     breed = models.CharField(max_length=100)
@@ -15,3 +22,29 @@ class Cat(models.Model):
         # Use the 'reverse' function to dynamically find the URL for viewing this cat's details
         return reverse('cat_detail', kwargs={'cat_id': self.id})
 
+class Feeding(models.Model):
+    date = models.DateField('Feeding Date')
+    meal = models.CharField(
+        max_length=1,
+        choices=MEALS, # add the 'choices' field option
+        default=MEALS[0][0] # set the default value for meal to be 'B'
+    )
+    cat = models.ForeignKey(Cat, on_delete=models.CASCADE)
+
+    def __str__(self):
+        # Nice method for obtaining the friendly value of a Field.choice
+        return f"{self.get_meal_display()} on {self.date}"
+    
+    class Meta:
+        ordering = ['-date']  # This line makes the newest feedings appear first
+
+# Add the Toy model
+class Toy(models.Model):
+    name = models.CharField(max_length=50)
+    color = models.CharField(max_length=20)
+
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return reverse('toy_detail', kwargs={'pk': self.id})
--- main_app/urls.py
+++ main_app/urls.py
@@ -12,6 +12,11 @@ urlpatterns = [
     path('cats/create/', views.CatCreate.as_view(), name='cat_create'),
     path('cats/<int:pk>/update/', views.CatUpdate.as_view(), name='cat_update'),
     path('cats/<int:pk>/delete/', views.CatDelete.as_view(), name='cat_delete'),
-
-
+    path('cats/<int:cat_id>/add-feeding/', views.add_feeding, name='add_feeding'),
+    path('toys/create/', views.ToyCreate.as_view(), name='toy_create'),
+    path('toys/<int:pk>/', views.ToyDetail.as_view(), name='toy_detail'),
+    path('toys/', views.ToyList.as_view(), name='toy_index'),
+        #Existing urls above
+    path('toys/<int:pk>/update/', views.ToyUpdate.as_view(), name='toy_update'),
+    path('toys/<int:pk>/delete/', views.ToyDelete.as_view(), name='toy_delete'),
 ]
